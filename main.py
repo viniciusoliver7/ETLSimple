d0222926d50164475d83711051a033bf582ad75f
viniciusoliver7 <103230924+viniciusoliver7@users.noreply.github.com>
2023-08-21 23:36:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ df=Spark.Spark(path="./archive(2)/Founders and Board Members.csv",format="csv",h
 df.select("Name","Companies")
 df.deleteNull("Companies")
 df.drop_duplicate("Companies")
-df.writeTableDBPostgres(urlDB="jdbc:postgresql://localhost:5432/vendas",nametable="testePYY",user="postgres",password="******")
+df.writeTableDBPostgres(urlDB="jdbc:postgresql://localhost:5432/db",nametable="testePYY",user="postgres",password="******")
 df.getDataFrame().show()
